bf15e9540f0993723319bc3fe659eff18bfe5858
shreesanjai <shreesanjai3@gmail.com>
2024-02-08 12:37:44 +0530

    All Correction Update

--- js/goToTop.js
+++ js/goToTop.js
@@ -1,16 +1,24 @@
-$(document).ready(function() {
-    // Show/hide the button based on scroll position
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) {
-            $('#goToTopBtn').fadeIn();
-        } else {
-            $('#goToTopBtn').fadeOut();
-        }
-    });
+let mybutton = document.getElementById("btn-back-to-top");
 
-    // Smooth scroll to top when button is clicked
-    $('#goToTopBtn').click(function() {
-        $('html, body').animate({scrollTop : 0}, 200);
-        return false;
-    });
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
