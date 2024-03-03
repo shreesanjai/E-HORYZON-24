c371013bb7bbda0dc3157ac8112e13192fbb96ba
Shree Sanjai <shreesanjai3@gmail.com>
2024-03-04 01:16:57 +0530

    /

--- js/countdown.js
+++ js/countdown.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const countdownText = document.querySelector(".countdown-text");
 
   const interval = setInterval(() => {
-    const deadline = new Date(2024, 2, 5, 23, 59, 59);
+    const deadline = new Date(2024, 2, 6, 23, 59, 59);
 
     const current = new Date();
 
